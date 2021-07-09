d888d2006fe105de0345bdf1557e550e43e43f11
rslewenstein <rafasoaresjf@gmail.com>
2021-07-09 20:05:00 -0300

    Finalizando goroutines e canais

---
+++ goroutines/multiplexacao/main.go
@@ -0,0 +1,35 @@
+// o SELECT funciona como um switch, mas para canais.
+// Ela bloqueia a execução do programa até receber um evento processado.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func process(ch chan string) {
+	time.Sleep(3 * time.Second)
+	ch <- "Done processing!"
+}
+
+func replicate(ch chan string) {
+	time.Sleep(1 * time.Second)
+	ch <- "Done replicating!"
+}
+
+func main() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	go process(ch1)
+	go replicate(ch2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case process := <-ch1:
+			fmt.Println(process)
+		case replicate := <-ch2:
+			fmt.Println(replicate)
+		}
+	}
+}
